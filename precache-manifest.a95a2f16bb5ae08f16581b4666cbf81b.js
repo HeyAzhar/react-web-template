3daff5f86e467dc7c8ea94538cf3f90752a25160
HeyAzhar <sk.azharuddin1230@gmail.com>
2020-04-20 01:06:46 +0530

    Updates

---
+++ precache-manifest.a95a2f16bb5ae08f16581b4666cbf81b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "533df5ac2495828a255823bca7402a5f",
+    "url": "/tic-tac/index.html"
+  },
+  {
+    "revision": "983472047e5692a42a76",
+    "url": "/tic-tac/static/css/main.3aef9a80.chunk.css"
+  },
+  {
+    "revision": "457904384500ea8f9ba9",
+    "url": "/tic-tac/static/js/2.154ff755.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac/static/js/2.154ff755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "983472047e5692a42a76",
+    "url": "/tic-tac/static/js/main.5c4128ce.chunk.js"
+  },
+  {
+    "revision": "86aa974c1cfc68afc1ec",
+    "url": "/tic-tac/static/js/runtime-main.5948d34b.js"
+  },
+  {
+    "revision": "81899e18586b10083308cdf1270c656f",
+    "url": "/tic-tac/static/media/avataaars.81899e18.svg"
+  },
+  {
+    "revision": "56ec4ee72a91e8f3d954db6cccbf8ab2",
+    "url": "/tic-tac/static/media/cabin.56ec4ee7.png"
+  },
+  {
+    "revision": "a29c0b43958bf3638364e15ff646fffa",
+    "url": "/tic-tac/static/media/cake.a29c0b43.png"
+  },
+  {
+    "revision": "e27c88e555910ddce50b6f1e275023a4",
+    "url": "/tic-tac/static/media/circus.e27c88e5.png"
+  },
+  {
+    "revision": "107d31e81b23d07ad38c7cc4d1f7ea27",
+    "url": "/tic-tac/static/media/game.107d31e8.png"
+  },
+  {
+    "revision": "7dfdcf559d331a0cdc05cc1d2b8c8233",
+    "url": "/tic-tac/static/media/safe.7dfdcf55.png"
+  },
+  {
+    "revision": "8fad4ff92040b17345896e0e777a79ad",
+    "url": "/tic-tac/static/media/submarine.8fad4ff9.png"
+  }
+]);
